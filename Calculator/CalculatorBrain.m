c4848586140fd793acd7064f1d62403229649640
rainer <rburgst@gmail.com>
2012-07-06 21:28:42 +0200

    integrated code from new assignment

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -11,75 +11,103 @@
 
 @interface CalculatorBrain () 
 
-@property (nonatomic, strong) NSMutableArray *operandStack;
+@property (nonatomic, strong) NSMutableArray *programStack;
 
 @end
 
 @implementation CalculatorBrain
 
-@synthesize operandStack = _operandStack;
+@synthesize programStack = _programStack;
 
-- (NSMutableArray *) operandStack {
-    if (!_operandStack) {
-        _operandStack = [[NSMutableArray alloc] init];
+- (NSMutableArray *) programStack {
+    if (!_programStack) {
+        _programStack = [[NSMutableArray alloc] init];
     }
-    return _operandStack;
-}
-
-- (double) popOperand {
-    NSNumber *last = [self.operandStack lastObject];
-    if (last != nil) {
-        [self.operandStack removeLastObject];
-    }
-    return [last doubleValue];
+    return _programStack;
 }
 
 - (void)pushOperand:(double)operand 
 {
-    [self.operandStack addObject:[NSNumber numberWithDouble:operand]];
+    [self.programStack addObject:[NSNumber numberWithDouble:operand]];
+}
+
+- (id) program {
+    return [self.programStack copy];
 }
 
 - (double) performOperation:(NSString *)operation 
 {
+    [self.programStack addObject:operation];
+    
+    return [self.class runProgram:self.programStack];
+}
+
+// if the top thing on the passed stack is an operand, return it
+// if the top thing on the passed stack is an operation, evaluate it (recursively)
+// does not crash (but returns 0) if stack contains objects other than NSNumber or NSString
+
++ (double)popOperandOffProgramStack:(NSMutableArray *)stack
+{
     double result = 0;
-    if ([@"+" isEqualToString:operation]) {
-        result = [self popOperand] + [self popOperand];
-    } else if ([@"-" isEqualToString:operation]) {
-        result = - [self popOperand] + [self popOperand];
-    } else if ([@"*" isEqualToString:operation]) {
-        result = [self popOperand] * [self popOperand];
-    } else if ([@"/" isEqualToString:operation]) {
-        double o1 = [self popOperand];
-        double o2 = [self popOperand];
-        // protect against divide by zero
-        if (o1 == 0) {
-            result = 0;
-        } else {
-            result = o2 / o1;
-        }
-    } else if ([@"sqrt" isEqualToString:operation]) {
-        result = sqrt([self popOperand]);
-    } else if ([@"π" isEqualToString:operation]) {
-        result = M_PI;
-    } else if ([@"sin" isEqualToString:operation]) {
-        result = sin([self popOperand]);
-    } else if ([@"cos" isEqualToString:operation]) {
-        result = cos([self popOperand]);
-    } else if ([@"+/-" isEqualToString:operation]) {
-        result = [self popOperand] * -1;
-    } else if ([@"log" isEqualToString:operation]) {
-        result = log([self popOperand]);
-    } else if ([@"ℯ" isEqualToString:operation]) {
-        result = exp([self popOperand]);
+    
+    id topOfStack = [stack lastObject];
+    if (topOfStack) {
+        [stack removeLastObject];
     }
     
-    // push the result back up the stack
-    [self pushOperand:result];
+    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+        result = [topOfStack doubleValue];
+    } else if ([topOfStack isKindOfClass:[NSString class]]) {
+        NSString *operation = topOfStack;
+        if ([@"+" isEqualToString:operation]) {
+            result = [self popOperandOffProgramStack:stack] + [self popOperandOffProgramStack:stack];
+        } else if ([@"-" isEqualToString:operation]) {
+            result = - [self popOperandOffProgramStack:stack] + [self popOperandOffProgramStack:stack];
+        } else if ([@"*" isEqualToString:operation]) {
+            result = [self popOperandOffProgramStack:stack] * [self popOperandOffProgramStack:stack];
+        } else if ([@"/" isEqualToString:operation]) {
+            double o1 = [self popOperandOffProgramStack:stack];
+            double o2 = [self popOperandOffProgramStack:stack];
+            // protect against divide by zero
+            if (o1 == 0) {
+                result = 0;
+            } else {
+                result = o2 / o1;
+            }
+        } else if ([@"sqrt" isEqualToString:operation]) {
+            result = sqrt([self popOperandOffProgramStack:stack]);
+        } else if ([@"π" isEqualToString:operation]) {
+            result = M_PI;
+        } else if ([@"sin" isEqualToString:operation]) {
+            result = sin([self popOperandOffProgramStack:stack]);
+        } else if ([@"cos" isEqualToString:operation]) {
+            result = cos([self popOperandOffProgramStack:stack]);
+        } else if ([@"+/-" isEqualToString:operation]) {
+            result = [self popOperandOffProgramStack:stack] * -1;
+        } else if ([@"log" isEqualToString:operation]) {
+            result = log([self popOperandOffProgramStack:stack]);
+        } else if ([@"ℯ" isEqualToString:operation]) {
+            result = exp([self popOperandOffProgramStack:stack]);
+        }
+    }
     
     return result;
 }
 
++ (NSString *)descriptionOfProgram:(id)program {
+    return @"TODO";
+}
+
++ (double)runProgram:(id)program {
+    NSMutableArray *stack;
+    
+    if ([program isKindOfClass:[NSArray class]]) {
+        stack = [program mutableCopy];
+    }
+    return [self.class popOperandOffProgramStack:stack];
+}
+
 - (void)clear {
-    self.operandStack = nil;
+    self.programStack = nil;
 }
 @end
